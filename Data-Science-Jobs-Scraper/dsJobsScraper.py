a72d475ab0ff75a68e4b106501360199efeddc1c
chizo <filip.cierkosz@gmail.com>
2022-04-06 23:39:52 +0200

    Refactored comments.

--- Data-Science-Jobs-Scraper/dsJobsScraper.py
+++ Data-Science-Jobs-Scraper/dsJobsScraper.py
@@ -1,34 +1,42 @@
-# Scrape latest Data Science job offers in London using Indeed API.
-# If using, remember to specify your browser's User-Agent (line 21).
-# Written by: Filip J. Cierkosz
-# Date: 01/2022
+'''
+Scrape latest Data Science job offers in London using Indeed API.
+
+Note: If using, remember to specify your browser's User-Agent (line 21).
+
+Author: Filip J. Cierkosz
+
+Date: 01/2022
+'''
 
 
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import datetime
-from requests.api import get
 
 
 # Array to store all the job items.
 joblist = []
 
 
-# Function 1: Get the data from the website.
 def getData():
+    '''
+    Gets the data from the website.
+    '''
     # Get the request from the specified website.
     headers = {'User-Agent':''} # <-- Specify your browser's User-Agent here!
     url = 'https://uk.indeed.com/jobs?q=data%20scientist%20%C2%A360%2C000&l=London%2C%20Greater%20London&vjk=8e4ebf6f8f6568f8'
     req = requests.get(url, headers)
 
-    # Create the soup object specifying the HTMl parser, then return it.
+    # Create the soup object specifying the HTML parser, then return it.
     soup = BeautifulSoup(req.content, 'html.parser')
     return soup
 
 
-# Function 2: Transform the soup object, and extract the desired contents.
 def refineData(soup):
+    '''
+    Transforms the soup object, and extract the desired contents.
+    '''
     # Find all the elements with the specified HTML tag and class name.
     divs = soup.find_all('div', class_='slider_item')
 
@@ -44,7 +52,7 @@ def refineData(soup):
         try:
             salary = item.find('div', class_='salary-snippet').text
         except:
-            salary = 'UNDEFINED'
+            salary = 'UNKNOWN'
 
         # Create a dictionary to store each data item, then append it in
         # the joblist array.
@@ -56,19 +64,22 @@ def refineData(soup):
         }
         joblist.append(job)
 
-    # Return the list that contains all the jobs offers.
     return joblist
 
 
-# Function 3 (optional): Clear the CSV datafile.
 def clearFile(fn):
+    '''
+    Optional usage. Clears the CSV datafile.
+    '''
     f = open(fn, 'w+')
     f.close()
-    print(f'The file {fn} has been cleared.')
+    print(f'The file {fn} has been cleared out.')
 
 
-# Function 4: Invoke all functions and store the data in an appropriate file.
 def main(fn):
+    '''
+    Invokes all functions and store the data in an appropriate file.
+    '''
     # Clear the CSV file (optional).
     #clearFile(fn)
 
@@ -83,12 +94,14 @@ def main(fn):
     # Create the datetime object of the current date and time.
     now = datetime.datetime.now()
 
-    # Confirm the successfull execution of the program in the terminal.
     print(f'''
-The data file has been successfully updated!
+---------------
+The datafile has been successfully updated!
 Date of last update: {now.strftime('%d/%m/%Y %H:%M:%S')}.
+---------------
 ''')
 
 
 # Test harness.
-if (__name__=='__main__'): main('jobs.csv')
+if (__name__=='__main__'): 
+    main('jobs.csv')
